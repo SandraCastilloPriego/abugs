6bc26949f6dd1860a72f78a04c3389af20623eeb
nolssi <nolssi@af01387e-86eb-b7d6-f347-9cf6c48f421e>
2010-10-24 14:07:23 +0000

    First classes

---
+++ trunk/src/abugs/bug/Gen.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010
+ * This file is part of XXXXXX.
+ * XXXXXX is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * XXXXXX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * XXXXXX; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package abugs.bug;
+
+/**
+ *
+ * @author bicha
+ */
+public class Gen {
+    String name;
+    double value;
+
+    public Gen(){
+        
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double value(){
+        return value;
+    }
+
+    public void Mutate(){}
+
+}
